a6c669f8ba1cea8528c31b0c1fc88ab79fd0f10e
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-22 13:08:43 -0400

    created see africa flash cards

---
+++ basics.js
@@ -0,0 +1,71 @@
+const flashCard = [{
+    title : "See Africa",
+    words:{
+        "Africa": "Africa has 54 countries and one “non-self governing territory”, the Western Sahara.",
+        "Egypt": "The Pharaonic civilization of ancient Egypt is one of the world’s oldest and longest-lasting civilizations.",
+        "The Sahara": "The Sahara is the largest desert in the world and is bigger than the continental USA.",
+        "Nigeria": "Nigeria is fourth largest oil exporter in the world, and Africa’s biggest oil producer with about 2.2 million barrels produced every day.",
+        "Lake Victoria": "Lake Victoria is the largest lake in Africa and the second-largest freshwater lake in the world.",
+        "Global Warming": "Africa has the most extensive biomass burning in the world, yet only emits about 4% of the world’s total carbon dioxide emissions.",
+        "Wild Life": "Africa has eight of the 11 major biomes and the largest-remaining populations of lion, elephant, rhinoceros, cheetah, hyena, leopard and hundreds of other species.",
+        "Megafauna": "Megafauna like giraffe, zebra, gorilla, hippopotamus, chimpanzee and wildebeest are unique to the continent.",
+        "Lake Malawi":"Lake Malawi has more fish species than any other freshwater system on earth.",
+        "WildLife Migration": "The Serengeti (Tanzania) hosts the world’s largest wildlife migration on Earth with over 750,000 zebra marching ahead of 1.2 million wildebeest as they cross this amazing landscape.",
+        "African elephant": "Africa is home to the world’s largest living land animal, the African elephant, which can weigh up to 7 tons.",
+        "The Nile": "The Nile River is the longest river in the world with a total length of 6,650 kilometres.",
+    }
+},
+{
+    title : "The Central Dogma",
+    words:{
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":""
+    }
+},
+
+{
+    title : "See Africa",
+    words:{
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":""
+
+    }
+},
+{
+    title : "See Africa",
+    words:{
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":"",
+        "":""
+    }
+}
+]
