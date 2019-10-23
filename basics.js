69906552cef768df8353be861224e01f29be8379
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-23 11:01:55 -0400

    re-did the js object and got them to sure in the browser

--- basics.js
+++ basics.js
@@ -1,71 +1,37 @@
-const flashCard = [{
-    title : "See Africa",
-    words:{
-        "Africa": "Africa has 54 countries and one “non-self governing territory”, the Western Sahara.",
-        "Egypt": "The Pharaonic civilization of ancient Egypt is one of the world’s oldest and longest-lasting civilizations.",
-        "The Sahara": "The Sahara is the largest desert in the world and is bigger than the continental USA.",
-        "Nigeria": "Nigeria is fourth largest oil exporter in the world, and Africa’s biggest oil producer with about 2.2 million barrels produced every day.",
-        "Lake Victoria": "Lake Victoria is the largest lake in Africa and the second-largest freshwater lake in the world.",
-        "Global Warming": "Africa has the most extensive biomass burning in the world, yet only emits about 4% of the world’s total carbon dioxide emissions.",
-        "Wild Life": "Africa has eight of the 11 major biomes and the largest-remaining populations of lion, elephant, rhinoceros, cheetah, hyena, leopard and hundreds of other species.",
-        "Megafauna": "Megafauna like giraffe, zebra, gorilla, hippopotamus, chimpanzee and wildebeest are unique to the continent.",
-        "Lake Malawi":"Lake Malawi has more fish species than any other freshwater system on earth.",
-        "WildLife Migration": "The Serengeti (Tanzania) hosts the world’s largest wildlife migration on Earth with over 750,000 zebra marching ahead of 1.2 million wildebeest as they cross this amazing landscape.",
-        "African elephant": "Africa is home to the world’s largest living land animal, the African elephant, which can weigh up to 7 tons.",
-        "The Nile": "The Nile River is the longest river in the world with a total length of 6,650 kilometres.",
-    }
-},
-{
-    title : "The Central Dogma",
-    words:{
-        "DNA": "DNA consists of very long molecules that contain nucleotide bases encoding genes.",
-        "Gene": "A sequence of DNA containing instructions for one protein (that may produce a trait; like eye color or lactose persistence).",
-        "Genome": "Total DNA sequence that contains the complete collection of genes that make up an organism's genetic information.",
-        "Chromosomes": "46 chromosomes, 23 pairs (including the sex chromosome). Females are XX and males are XY.",
-        "Karyotype": "The number, arrangement and size of chromosome in a particular organism.",
-        "The Central Dogma of Molecular Biology": "DNA-->mRNA-->Protein",
-        "Steps of the Central Dogma": "Replication, Transcription, Translation",
-        "Intron": "Intervening sequence in an RNA primary transcript and it's edited out (removed)",
-        "Exon":"Expressed sequence, part of the mRNA that is expressed as part of the protein",
-        "Messenger RNA": "A continuous chain of codons that specify a specifies amino acids to be included in the protein.",
-        "Translation": "Translate the mRNA message into a protein sequence",
-        "Transcription":"Transcribe one strand of the DNA as a message"
-    }
-},
+const words = ["Africa","Egypt","The Sahara","Nigeria","Lake Victoria","Global Warming","Wild Life","Megafauna","Lake Malawi","WildLife Migration","African elephant","The Nile"]
+const definitions = ["Africa has 54 countries and one “non-self governing territory”, the Western Sahara.", "The Pharaonic civilization of ancient Egypt is one of the world’s oldest and longest-lasting civilizations.", "The Sahara is the largest desert in the world and is bigger than the continental USA.", "Nigeria is fourth largest oil exporter in the world, and Africa’s biggest oil producer with about 2.2 million barrels produced every day.", "Lake Victoria is the largest lake in Africa and the second-largest freshwater lake in the world.",
+"Africa has the most extensive biomass burning in the world, yet only emits about 4% of the world’s total carbon dioxide emissions.",
+"Africa has eight of the 11 major biomes and the largest-remaining populations of lion, elephant, rhinoceros, cheetah, hyena, leopard and hundreds of other species.",
+"Megafauna like giraffe, zebra, gorilla, hippopotamus, chimpanzee and wildebeest are unique to the continent.",
+"Lake Malawi has more fish species than any other freshwater system on earth.",
+"The Serengeti (Tanzania) hosts the world’s largest wildlife migration on Earth with over 750,000 zebra marching ahead of 1.2 million wildebeest as they cross this amazing landscape.",
+"Africa is home to the world’s largest living land animal, the African elephant, which can weigh up to 7 tons.",
+"The Nile River is the longest river in the world with a total length of 6,650 kilometres."]
 
-{
-    title : "Women Inventors",
-    words:{
-        "MediciJeanne Villepreux-Power": "The aquarium",
-        "Josephine Cochrane": "The dishwasher",
-        "Madam C. J. Walker": "Hair care products",
-        "Grace Hopper": "The Computer",
-        "Marie Van Brittan Brown":"home security systems,",
-        "Patricia Bath":"Laserphaco Probe",
-        "Marie Curie":"Theory of Radioactivity",
-        "Rosalind Franklin":"Rosalind Franklin",
-        "Dr. Shirley Jackson:":"Modern Day Telecommunication",
-        "Maria Beasley":"The Life Raft",
-        "Katharine Burr Blodgett":"Non-reflective Glass",
-        "Hedy Lamarr":"Frequency Hopping” Technology"
+function createFlashCard(){
+const carddiv = document.querySelector(".cardbox");
+const cardBox = document.createElement("div")
+cardBox.classList.add("card");
+cardBox.setAttribute("id", "showword");
+carddiv.appendChild(cardBox);
 
-    }
-},
-{
-    title : "See Africa",
-    words:{
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":"",
-        "":""
-    }
+for (let i = 0; i < words.length; i++) {
+let frontText = words[i] 
+const wordSide = document.createElement("div")
+wordSide.classList.add("flashcard");
+wordSide.setAttribute("id", "front-card");
+wordSide.textContent = frontText
+cardBox.appendChild(wordSide);
 }
-]
+
+for (let j = 0; j < definitions.length; j++) {
+let backText = definitions[j]
+const definitionSide = document.createElement("div")
+definitionSide.classList.add("flashcard");
+definitionSide.setAttribute("id", "definition");
+definitionSide.textContent = backText
+cardBox.appendChild(definitionSide);
+}
+return(cardBox)
+}
+
