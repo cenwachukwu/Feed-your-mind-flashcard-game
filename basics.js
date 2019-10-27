292465702f678f6affc9845ab069bfb10b41028c
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-27 12:33:03 -0400

    made a next button that works with all the deck of flashcards

--- basics.js
+++ basics.js
@@ -42,35 +42,111 @@ let flashCards = [{
     "Katharine Burr Blodgett": "Non-reflective Glass"
 },
 {
-    // title : "See Africa",
-    // words:{
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":"",
-    //     "":""
-    // }
+    
+    "NH4+":"Ammonium",
+    "OH-":"Hydroxide",
+    "ClO3-":"Chlorate",
+    "NO3-":"Nitrate",
+    "NO2-":"Nitrite",
+    "C2H3O2-":"Acetate",
+    "CN-":"Cyanide",
+    "K":"Potassium, 19, Metal",
+    "Sc":"Scandium, 21, Metal",
+    "Ti":"Titanium, 22, Metal",
+    "V":"Vanadium, 23, Metal",
+    "Cr":"Chromium, 24, Metal"
+},
+{
+    "Learning":"relatively permanent change in knowledge/behavior arising from experience",
+    "Classical Conditioning":"we learn to associate two stimuli and thus to anticipate events",
+    "Pavlov":"classical conditioning, putting food in a dogs mouth caused the animal to salivate. dog began salivating at the sight of food",
+    "Unconditioned Response":"naturally occurring response to the unconditioned stimulus (salvation in response to food)",
+    "Unconditioned Stimulus":"stimulus that unconditionally (naturally and automatically) triggers a response (food in mouth triggers salvation)",
+    "Conditioned Response":"the learned response to a previously neutral stimulus (salvation in response to the tone was conditional upon the dog's learning the association between the tone and food)",
+    "Conditioned Stimulus":"an originally irrelevant stimulus that, after association with an unconditioned stimulus, comes to trigger a conditioned response (tone stimulus now triggers the conditional salvation)",
+    "Little Albert":"taught to be afraid of things that are white. he sees a white rat, they bang a metal pole and he gets afraid. they do it continuously and he eventually starts crying at the sight of the rat.",
+    "Tolerance":"diminished response to a drug after repeated use",
+    "Clever Hans":"horse that learned to do math by picking up on signals from his master",
+    "Extinction":"the diminished responding that occurs when the conditioned stimulus (tone) no longer signals the impending unconditioned stimulus (food)",
+    "Spontaneous Recovery":"the reappearance of the conditioned response triggered by the conditioned stimulus after a pause"
+},
+{
+    "Phylogeny":"The evolutionary history of a species or group of related species",
+    "Systematics":"A discipline focused on classifying organisms and determining their evolutionary relationships",
+    "Taxonomy":"How organisms are named and classified by their characteristics",
+    "Binomial nomenclature":"Classification system in which each species is assigned a two-part scientific name",
+    "Taxonomic groups":"Domain kingdom phylum class order family genus species",
+    "Taxon":"A taxonomic unit at any level of hierarchy",
+    "Phylogenetic tree":"A branching diagram that represents the evolutionary history of a group of organisms",
+    "Phylocode":"A classification system which recognizes only groups that include a common ancestor and all its descendants",
+    "Analogies":"Similarities due to convergent evolution",
+    "Cladistics":"The approach to systematics in which common ancestry is the primary criterion used to classify organisms",
+    "Clade":"A monophyletic group of species that includes an ancestral species and all its descendants",
+    "Paraphyletic":"A non-clade group which consists of an ancestral species and some of its descendants"
+},
+{
+    "Konyak tribal women":"Konyak tribal women participate to set a Guinness World Record for the 'largest traditional Konyak dance', in Kohima on April 05, 2019.",
+    "Jyoti Amge":"Standing 24.7 inches tall, Amge has held the title of the Shortest Living Woman since her 18th birthday on December 16, 2011.",
+    "Chris Walton":"Her nails measure 10 feet 2 inches on her left hand and 9 feet 7 inches on her right hand that she has been growing for 18 years.",
+    "Marjorie Gestring":"Gestring won the Olympic gold for springboard diving in 1936 at just under the age of 14. Her record still stands today.",
+    "Teresa Edwards":"Edwards has four gold Olympic medals and one bronze for basketball, while the top male athlete, Carmelo Anthony, only has three gold and one bronze.",
+    "Margaret Court":"Margaret Court holds 24 titles, seven more than the top male record holder, Roger Federer. Steffi Graf, Serena Williams, Helen Wills Moody, Chris Evert, and Martina Navratilova all have more than Federer, too.",
+    "Johanna Quaas":"She is the world’s oldest gymnast. In 2012, at age 86, she took part in the competition that would put her in the record books. She is now 91 and still hitting the gym.",
+    "Paddy Jones":"Jones, 76, holds the Guinness Record for the oldest salsa dancer in the world.",
+    "Carli Lloyd":"Lloyd scored three goals, which is called a hat trick, in just 16 minutes of the 2015 Women’s World Cup game. ",
+    "Jeanne Louise Calment":"The oldest recorded age anyone ever reached was 122 years and 164 days. Jeanne Louise Calment was born on February 21, 1875, and died on August 4, 1997.",
+    "Stéphanie Gicquel":"Gicquel crossed the surface of chilling Antarctica at a total distance of 2,045 km (1270.7 miles) without the help of wind kites, motors or dogs.",
+    "Peggy Whitson":"Peggy Whitson launched into space at the age of 56, making her the oldest female astronaut;ß still eager to take on missions sparked from her passion for science"
+},
+{
+    "Angela Merkel":"Chancellor of Germany",
+    "Ellen Johnson Sirleaf":"President of Liberia",
+    "Aung San Suu Kyi":"State Counsellor of Myanmar",
+    "Michelle Bachelet":"President of Chile",
+    "Judy Smith":"Crisis Manager (Real life Olivia Pope), United States",
+    "Janet Yellen":"Chair of the Federal Reserve, United States",
+    "Helle Thorning-Schmidt":"Former Prime Minister of Denmark",
+    "Loretta Lynch":"Former Attorney General, United States",
+    "Park Geun-hye":"President of South Korea",
+    "Arundhati Bhattacharya":"Chairperson of the State Bank of India",
+    "Sonia Gandhi":"President of Indian National Congress",
+    "Christine Lagarde":"Managing director, International Monetary Fund",
+    "Tsai Ing-Wen":"President of Taiwan",
+    "Theresa May":"Prime Minister of the United Kingdom",
+    "Cristina Fernandez de Kirchner":"Former President of Argentina",
+    "Sheikh Hasina Wajed":"Prime Minister of Bangladesh",
+    "Erna Solberg":"Prime Minister of Norway",
+    "Dalia Grybauskaitė":"President of Lithuania",
+    "Ameenah Gurib-Fakim":"President of Mauritius",
+    "Ameenah Gurib-Fakim":"",
+    "Beata Szydło":"Prime Minister of Poland",
+    "Saara Kuugongelwa-Amadhila":"Prime Minister of Namibia"
+},
+{
+    
+    "Jenny Thompson":"Swimming: 12 medals (8 Gold, 3 Silver, 1 Bronze)",
+    "Dara Torres":"Swimming: 12 medals (4 Gold, 4 Silver, 4 Bronze)",
+    "Natalie Coughlin":"Swimming: 12 medals (3 Gold, 4 Silver, 5 Bronze)",
+    "Allyson Felix":"Track and Field: 9 medals (6 Gold, 3 Silver)",
+    "Allison Schmitt":"Swimming: 8 medals (4 Gold, 2 Silver, 2 Bronze)",
+    "Shirley Babashoff":"Swimming: 8 medals (2 Gold, 6 Silver)",
+    "Dana Vollmer":"Swimming: 7 medals (5 Gold, 1 Silver, 1 Bronze)",
+    "Amanda Beard":"Swimming: 7 medals (2 Gold, 4 Silver, 1 Bronze)",
+    "Shannon Miller":"Gymnastics: 7 medals (2 Gold, 2 Silver, 2 Bronze)",
+    "Amy Van Dyken":"Swimming: 6 medals (6 Gold)",
+    "Katie Ledecky":"Swimming: 6 medals (5 Gold, 1 Silver)",
+    "Bonnie Blair":"Long Track Speedskating: 6 medals (5 Gold, 1 Bronze)"
+   
 }
 ]
-let i = 0 //this is serving as a constant for our index as we are going to use this to manipultae the dom
+let i = 0 //this is serving as a constant for our index as we are going to use this to manipulate the dom
 let j = 0
-
-// next = i + 1
-// previous = i - 1
 
 function nextItem() {
     i = i + 1; // increase i by one
     i = i % flashCard.length; // if we've gone too high, start from `0` again
     return flashCard[i]; // give us back the item of where we are now
 }
-
 function prevItem() {
     if (i === 0) { // i would become 0
         i = flashCard.length; // so put it at the other end of the array
@@ -78,14 +154,13 @@ function prevItem() {
     i = i - 1; // decrease by one
     return flashCard[i]; // give us back the item of where we are now
 }
+function loadCard() {
+    frontCard.textContent = flashCard[i][0]  //now you can access the array index and make the text content of the front and back card different things 
+    backCard.textContent = flashCard[i][1]
+}
 function toggle(){
 
 }
-
-// box1.disabled = true
-//     femaleInventors.disabled = true
-//     centraldogma.disabled = true
-
 
 const cardContainer = document.querySelectorAll(".cardcontainer")
 // const frontCard = document.querySelector(".front-card")
@@ -98,9 +173,25 @@ const box2a = document.querySelector(".box2a")
 const box2b = document.querySelector(".box2b")
 const box3 = document.querySelector(".box3")
 const box3a = document.querySelector(".box3a")
-const box3b = document.querySelector(".box3b") 
-// const women = document.querySelector(".women")
-// const buttons = document.querySelector(".buttons")
+const box3b = document.querySelector(".box3b")
+const box4 = document.querySelector(".box4")
+const box4a = document.querySelector(".box4a")
+const box4b = document.querySelector(".box4b")
+const box5 = document.querySelector(".box5")
+const box5a = document.querySelector(".box5a")
+const box5b = document.querySelector(".box5b")
+const box6 = document.querySelector(".box6")
+const box6a = document.querySelector(".box6a")
+const box6b = document.querySelector(".box6b") 
+const box7 = document.querySelector(".box7")
+const box7a = document.querySelector(".box7a")
+const box7b = document.querySelector(".box7b")
+const box8 = document.querySelector(".box8")
+const box8a = document.querySelector(".box8a")
+const box8b = document.querySelector(".box8b")
+const box9 = document.querySelector(".box9")
+const box9a = document.querySelector(".box9a")
+const box9b = document.querySelector(".box9b") 
 const next = document.querySelector(".next")
 const previous = document.querySelector(".previous")
 const remove = document.querySelector("button")
@@ -128,88 +219,114 @@ remove.addEventListener("click", function(evt){
 
 
 box1.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
     flashCard = Object.entries(flashCards[j]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
-    box1a.textContent = flashCard[i][0]  //now you can access the array index and make the text content of the front and back card different things 
-    box1b.textContent = flashCard[i][1]
+    frontCard = box1a
+    backCard = box1b
+    loadCard()
     next.classList.remove("inactive");
-    
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box2.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 1]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box2a
+    backCard = box2b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box3.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 2]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box3a
+    backCard = box3b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box4.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 3]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box4a
+    backCard = box4b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box5.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 4]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box5a
+    backCard = box5b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box6.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 5]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box6a
+    backCard = box6b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box7.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 6]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box7a
+    backCard = box7b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box8.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 7]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box8a
+    backCard = box8b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
+})
+box9.addEventListener("click", function(evt){
+    evt.preventDefault
+    console.log(evt)
+    flashCard = Object.entries(flashCards[j, 8]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
+    frontCard = box9a
+    backCard = box9b
+    loadCard()
+    next.classList.remove("inactive");
+    console.log(evt.target.children[0].textContent)
+    console.log(evt.target.children[1].textContent)
 })
 
 next.addEventListener("click", function(evt){
     evt.preventDefault
     nextItem()
-    // flashCard = Object.entries(flashCards[j])
-    box1a.textContent = flashCard[i][0]
-    box1b.textContent = flashCard[i][1]
+    loadCard()
     previous.classList.remove("inactive");
 })
 
 previous.addEventListener("click", function(evt){
     evt.preventDefault
     prevItem()
-    box1a.textContent = flashCard[i][0]
-    box1b.textContent = flashCard[i][1]
+    loadCard()
 })
-
-//how to unappend elements from the dom/how to remove an elemnet through dom manioulation
-
-box2.addEventListener("click", function(evt){ 
-    flashCard = Object.entries(flashCards[j, 1]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
-    box2a.textContent = flashCard[i][0]  //now you can access the array index and make the text content of the front and back card different things 
-    box2b.textContent = flashCard[i][1]
-    next.classList.remove("inactive");
-    
-})
-
-// next.addEventListener("click", function(evt){
-//     evt.preventDefault
-//     nextItem()
-//     // flashCard = Object.entries(flashCards[j,1])
-//     frontCard2.textContent = flashCard[i][0]
-//     backCard2.textContent = flashCard[i][1]
-//     previous.classList.remove("inactive");
-// })
-
-// previous.addEventListener("click", function(evt){
-//     evt.preventDefault
-//     prevItem()
-//     // flashCard = Object.entries(flashCards[j,1])
-//     frontCard2.textContent = flashCard[i][0]
-//     backCard2.textContent = flashCard[i][1]
-// })
-
-// femaleInventors.addEventListener("click", function (evt) {
-//     flashCard = Object.entries(flashCards[j, 2]) //makes the object into an array so you're able to access it, remember that the value of i and j is 0. 
-//     frontCard3.textContent = flashCard[i][0]  //now you can access the array index and make the text content of the front and back card different things 
-//     backCard3.textContent = flashCard[i][1]
-//     next.classList.remove("inactive");
-//     women.classList.remove("inactive")
-
-// })
-
-
-
-// next.addEventListener("click", function (evt) {
-//     evt.preventDefault
-//     nextItem()
-//     flashCard = Object.entries(flashCards[j, 2])
-//     frontCard3.textContent = flashCard[i][0]
-//     backCard3.textContent = flashCard[i][1]
-//     previous.classList.remove("inactive");
-// })
-
-// previous.addEventListener("click", function (evt) {
-//     evt.preventDefault
-//     prevItem()
-//     flashCard = Object.entries(flashCards[j, 2])
-//     frontCard3.textContent = flashCard[i][0]
-//     backCard3.textContent = flashCard[i][1]
-// })
-
-
-
-
-
-
-
-// flashCard = Object.entries(flashCards[j, 1])
